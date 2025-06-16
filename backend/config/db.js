49429adf5c2ecaa64bd53afa4dd276f950331e0e
NisalMallawaarachchi <nisalmallawarachchi@gmail.com>
2025-06-16 18:02:56 +0530

    feat: implement MongoDB connection and update server initialization

---
+++ backend/config/db.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+    try {
+        // The `await` keyword pauses execution until the promise is resolved.
+        const conn = await mongoose.connect(process.env.MONGODB_URI);
+        console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.error(`❌ MongoDB connection error: ${error.message}`);
+        process.exit(1); // process code 1 means exit with failure and 0 means exit with success
+    }
+};
+
+export default connectDB;
