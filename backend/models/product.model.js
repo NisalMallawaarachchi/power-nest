0c17892518a648bd8b82b59488f685006558244b
NisalMallawaarachchi <nisalmallawarachchi@gmail.com>
2025-06-16 18:27:01 +0530

    feat: add product model with schema definition

---
+++ backend/models/product.model.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    Image: {
+        type: String,
+        required: false,
+        default: 'https://via.placeholder.com/150'
+    },
+}, {
+    timestamps: true, // Automatically manage createdAt and updatedAt fields
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+export default Product;
